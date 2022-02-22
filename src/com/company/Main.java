126e0922d8aeeff267320ec34ca99b16ce1095d1
TasmanianNoob <charlierc2003@gmail.com>
2022-02-22 20:05:48 +1100

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, I have a small dong.");
+        System.out.println("Hello, I have a small hand.");
 	// write your code here
     }
 }
