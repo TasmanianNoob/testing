4481f4456faf185376d4d717a309d49a0b58a9e0
charlie <charlierc2003@gmail.com>
2022-02-22 19:43:41 +1100

    test 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello!!!!");
+        System.out.println("Hello, I have a small dong!!!!");
 	// write your code here
     }
 }
